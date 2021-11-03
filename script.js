4141f99a335168b850762c81f2739a685bea0ffb
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-11-03 13:22:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let n = prompt("Number of pizzas");
-n -= 1;
 let order = prompt("Your order");
 function orderingPizza(order, n) {
   let pizzas = ["Pizza with mushrooms", "Pizza with chicken and pinapple"];
   let str = "Your order:";
   if (n == 0) {
-    str += pizzas[order] + " " + 2;
-    alert(str);
+    if (order == 0) {
+      str += pizzas[1] + " " + 1;
+      alert(str);
+    } else {
+      str += pizzas[order] + " " + 1;
+      alert(str);
+    }
   } else if (isNaN(n) || isNaN(order)) {
     alert("input number");
   } else {
-    n += 1;
     str += pizzas[order] + " " + n;
     alert(str);
   }
